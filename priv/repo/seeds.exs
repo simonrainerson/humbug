120df089754ef35dd0d69215b49dcc1865e80d58
Simon Rainerson <simonr88@gmail.com>
2023-07-31 02:32:59 +0200

    🔧 Add database seed for development

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,28 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+require Ecto.Query
+user1 = Humbug.Repo.insert!(%Humbug.Users.User{name: "Chip Munk"})
+user2 = Humbug.Repo.insert!(%Humbug.Users.User{name: "Corey Ander"})
+user3 = Humbug.Repo.insert!(%Humbug.Users.User{name: "Molly Kuehl"})
+
+{:ok, room1} =
+  Humbug.Discussions.create_room(%{
+    name: "Stuff",
+    owner_id: 1
+  })
+
+{:ok, room2} =
+  Humbug.Discussions.create_room(%{
+    name: "Things",
+    owner_id: 2
+  })
+
+{:ok, room3} =
+  Humbug.Discussions.create_room(%{
+    name: "Moar Things",
+    owner_id: 2
+  })
+
+Humbug.Discussions.add_users_to_room(room3, [user1, user2, user3])
